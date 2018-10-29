fad3a7eefdc047e08124c9a8ee8ec04d62663976
momocirius <44583258+momocirius@users.noreply.github.com>
2018-10-29 21:00:31 +0100

    Initial commit

---
+++ question1/Testcase.java
@@ -0,0 +1,14 @@
+package question1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Testcase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
